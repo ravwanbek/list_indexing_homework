17d9e3b4d1910f4e09a5b82336eef779b52782b2
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-10 20:22:54 +0900

    10

--- List10.py
+++ List10.py
@@ -6,4 +6,9 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+
+    if list_num[0]>=list_num[-1]:
+       return list_num[0]
+    else: return list_num[1]
+print(main([10,5,8,9,6]))
+    
