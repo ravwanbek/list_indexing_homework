8c68f63a7131cf561f86abee7f7ddea732d3b522
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-10 20:12:13 +0900

    6

--- List06.py
+++ List06.py
@@ -6,4 +6,21 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    
+
+    if list1[0]==1:
+        list1[0]=True
+    if list1[1]==1:
+        list1[1]=True
+    if list1[2]==1:
+        list1[2]=True
+    if list1[3]==1:
+        list1[3]=True
+    if list1[4]==1:
+        list1[4]=True
+    
+    
+
+
+    return list1
+print(main([0,1,1,0,1]))
