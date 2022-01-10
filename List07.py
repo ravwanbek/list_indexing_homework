02750782a9c447d17d67ba5889634497c34a651e
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-10 20:12:24 +0900

    7

--- List07.py
+++ List07.py
@@ -6,4 +6,17 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    if list1[0]==0:
+        list1[0]=False
+    if list1[1]==0:
+        list1[1]=False
+    if list1[2]==0:
+        list1[2]=False
+    if list1[3]==0:
+        list1[3]=False
+    if list1[4]==0:
+        list1[4]=False
+    
+    
+    return list1
+print(main([0,1,1,0,0]))
